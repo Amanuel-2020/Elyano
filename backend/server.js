10324eae3b73c561dbbc03554b58dd4bec56cf04
amanuel <amanuelwakshum2020@gmail.com>
2021-09-29 14:57:15 +0300

    fixed production level

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const app = require('./app')
 const ConnectDB = require('./database/database');
-// const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 
 // setting up config file
-// if(process.env.NODE_ENV === 'PRODUCTION') require('dotenv').config({path: 'backend/config/config.env'})
+if(process.env.NODE_ENV === 'PRODUCTION') require('dotenv').config({path: 'backend/config/config.env'})
 
 // connecting database
 ConnectDB();
